fcbf0390b8545c102c8805f7f56b4b3a10a69c6e
kiseitai2 <kiseitai2@yahoo.com>
2014-08-11 23:52:04 -0400

    Added Plugin via dynamic library support!

--- plugin.h
+++ plugin.h
@@ -1,6 +1,48 @@
 #ifndef PLUGIN_H_INCLUDED
 #define PLUGIN_H_INCLUDED
+#include <iostream>
+#include <string>
+#include "typedefs.h"
 
+//For compatibility purposes, I looked up the compiler os detection macros at
+//http://nadeausoftware.com/articles/2012/01/c_c_tip_how_use_compiler_predefined_macros_detect_operating_system#Howtodetecttheoperatingsystemtype
+
+#if defined(__linux__) || defined(__APPLE__)//Under unix systems
+#include <dlfcn.h>
+#define openLib dlopen
+#define loadF dlsym
+#define closeLib dlclose
+#define lib_ptr void_ptr
+#define fcn_ptr void_ptr
+
+#elif defined(__CYGWIN__)//Under unix in Windows
+#include <dlfcn.h>
+#define openLib dlopen
+#define loadF dlsym
+#define closeLib dlclose
+#define lib_ptr void_ptr
+#define fcn_ptr void_ptr
+
+#elif defined(__WIN32__) || defined(__WIN64__)//Under Windows systems
+#include <windows.h>
+#define openLib LoadLibrary
+#define loadF GetProcAddress
+#define closeLib FreeLibrary
+#define lib_ptr HMODULE
+#define fcn_ptr FARPROC
+#endif
+
+class Plugin
+{
+public:
+    typedef void (*func_ptr)(...);
+    Plugin(const char* file);//Load the module!
+    func_ptr GetFunction(const std::string& name);//Extract a pointer to a function within the library!
+    bool isLoaded() const;//Let us know if the library loaded!
+    ~Plugin();
+private:
+    lib_ptr lib;
+};
 
 
 #endif // PLUGIN_H_INCLUDED
